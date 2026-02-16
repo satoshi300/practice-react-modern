57fefe0ab76020a3a06b945897a96e8897c4149b
Michal Wasiak <michal.wasiak@hotmail.com>
2026-02-16 18:04:25 +1000

    Task 02 done

--- 02/components/App.js
+++ 02/components/App.js
@@ -1,16 +1,21 @@
 // ./src/components/App.js
 import React from 'react';
-import Box from './Box';
+import TextContext from '../context';
+import Div from './Div';
 
 class App extends React.Component {
     state = {
-        text: 'React HelloWorld Modern!',
+        text: 'Hello from context',
     };
 
     render() {
         const { text } = this.state;
 
-        return <Box text={text} />;
+        return (
+            <TextContext.Provider value={text}>
+                <Div />
+            </TextContext.Provider>
+        );
     }
 }
 
