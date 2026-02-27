1c620930340950d8b5917cc3803a41dc547abe5a
Michal Wasiak <michal.wasiak@hotmail.com>
2026-02-27 22:10:33 +1000

    Task 04 basic functiionality

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,8 +1,34 @@
-import React, { useEffect } from 'react';
+/* eslint-disable */
+import React, { useState, useEffect, useRef } from 'react';
 import useRandomItem from './hook';
 
 function SpeedTest() {
+    const [counter, setCounter] = useState(0);
+    const [timer, setTimer] = useState(0);
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
+    const inputWord = useRef(null);
+    const idInterval = useRef(null);
+
+    const start = () => {
+        idInterval.current = setInterval(() => {
+            setTimer((value) => value + 1);
+        }, 1000);
+    };
+
+    const stop = () => {
+        if (idInterval.current) {
+            clearInterval(idInterval.current);
+        }
+    }
+
+    const handleChange = () => {
+        console.log(inputWord.current.value)
+        if (inputWord.current.value === word) {
+            setCounter((value) => value + word.length);
+            regenerateWord();
+            inputWord.current.value = '';
+        }
+    }
 
     useEffect(() => {
         regenerateWord();
@@ -11,7 +37,9 @@ function SpeedTest() {
     return (
         <div>
             <h1>{word}</h1>
-            <input />
+            <div>{timer} s</div>
+            <div>{counter}</div>
+            <input name="word" ref={inputWord} onBlur={stop} onFocus={start} onChange={handleChange} />
         </div>
     );
 }
